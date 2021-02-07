62b4b61f3ea0fc1bc717a4c977ce0aae6a119e94
Po-Ting-lin <c1c4c5c6@gmail.com>
2021-02-07 18:51:33 +0800

    clean redundent functions

--- PhaseRetrieval/utils.h
+++ PhaseRetrieval/utils.h
@@ -6,37 +6,6 @@ inline int iDivUp(int a, int b)
     return (a % b != 0) ? (a / b + 1) : (a / b);
 }
 
-//Align a to nearest higher multiple of b
-inline int iAlignUp(int a, int b)
-{
-    return (a % b != 0) ? (a - a % b + b) : a;
-}
-
-// Rounding up the FFT dimensions to the next power of 2,
-// unless the dimension would exceed 1024, 
-// in which case it's rounded up to the next multiple of 512.
-// Reference: zchee -- https://github.com/zchee/cuda-sample
-static int snapTransformSize(int dataSize) {
-    int hiBit;
-    unsigned int lowPOT, hiPOT;
-
-    dataSize = iAlignUp(dataSize, 16);
-    for (hiBit = 31; hiBit >= 0; hiBit--) {
-        if (dataSize & (1U << hiBit)) {
-            break;
-        }
-    }
-    lowPOT = 1U << hiBit;
-
-    if (lowPOT == (unsigned int)dataSize)
-        return dataSize;
-    hiPOT = 1U << (hiBit + 1);
-    if (hiPOT <= 1024)
-        return hiPOT;
-    else
-        return iAlignUp(dataSize, 512);
-}
-
 static void printTime(std::chrono::system_clock::time_point t1, std::chrono::system_clock::time_point t2, std::string name) {
     std::chrono::duration<double> time_lapse = t2 - t1;
     std::cout << name << " time consume: " << time_lapse.count() << " s" << std::endl;
